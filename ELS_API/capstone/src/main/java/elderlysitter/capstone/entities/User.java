7ee7cd0064426e2a7207b85083690febd2c953cf
ttson1001 <ttson1001@gmail.com>
2022-09-13 13:04:14 +0700

    [ELS_1] Set up SpringBoot

---
+++ ELS_API/capstone/src/main/java/elderlysitter/capstone/entities/User.java
@@ -0,0 +1,59 @@
+package elderlysitter.capstone.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Column(name = "Full_name")
+    private String fullName;
+
+    private LocalDate dob;
+
+    private String gender;
+
+    private String phone;
+
+    private String address;
+
+    private String email;
+
+    @Column(name = "create_date")
+    private LocalDate createDate;
+
+    private String status;
+
+    private String identity_card;
+
+    private String skill;
+
+    @JsonIgnore
+    @Column(name = "role_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Role role;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Certificate> certificates;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Elder> elders;
+}
