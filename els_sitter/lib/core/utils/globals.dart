9689703d25c2300f37993ce50a150b37f7e9f9e8
khanhpl <khanh.phanluu01@gmail.com>
2022-10-23 14:11:32 +0700

    khanhpl update load image

--- els_sitter/lib/core/utils/globals.dart
+++ els_sitter/lib/core/utils/globals.dart
@@ -1,7 +1,11 @@
 library els_cus_mobile.globlas;
 
+import 'dart:io';
+
 import 'package:els_sitter/core/models/user_model.dart';
 
 
 
 UserModel? curUser ;
+bool isIDFrontCheck = false;
+File idFrontFile = File('');
