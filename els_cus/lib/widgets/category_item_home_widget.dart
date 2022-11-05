36642b8c79bfafcdcdcfa1699b64586d157403c0
binhquyen5769 <ngobinhkhien@gmail.com>
2022-11-05 09:22:59 +0700

    category

--- els_cus/lib/widgets/category_item_home_widget.dart
+++ els_cus/lib/widgets/category_item_home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:els_cus_mobile/core/models/category_data_model.dart';
+import 'package:els_cus_mobile/widgets/service_item_home_widget.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItemHomeWidget extends StatelessWidget {
@@ -8,12 +9,12 @@ class CategoryItemHomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: GestureDetector(
-        onTap: (){
-          Navigator.pushNamed(context, "/abc");
-        },
+    return GestureDetector(
+      onTap: (){
+        // Navigator.push(context, MaterialPageRoute(builder: (context) => ServiceItemHomeWidget(service: service)))
+      },
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
         child: Container(
           alignment: Alignment.center,
           height: size.height*0.1,
--- els_cus/lib/widgets/service_item_home_widget.dart
+++ els_cus/lib/widgets/service_item_home_widget.dart
@@ -1,104 +1,110 @@
-// import 'package:flutter/material.dart';
-//
-// import '../core/models/service_data_model.dart';
-// import '../core/utils/color_constant.dart';
-//
-// class ServiceItemHomeWidget extends StatelessWidget{
-//   ServiceDataModel service;
-//   ServiceItemHomeWidget({super.key, required this.service});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     var size = MediaQuery.of(context).size;
-//     return Align(
-//       alignment: Alignment.centerLeft,
-//       child: Padding(
-//         padding: EdgeInsets.only(
-//         top: size.height*0.01,
-//         bottom: size.height*0.005,
-//         left: size.width*0.03,
-//         ),
-//       child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         mainAxisSize: MainAxisSize.max,
-//         children: [
-//           Container(
-//             width: 60,
-//             height: 60,
-//             padding: EdgeInsets.only(
-//               left: size.width*0.03,
-//               top: size.height*0.01,
-//               right: size.width*0.03,
-//               bottom: size.height*0.01,
-//               ),
-//             decoration: BoxDecoration(
-//               color: ColorConstant.gray400,
-//               borderRadius: BorderRadius.circular(
-//               40,
-//               ),
-//             ),
-//           ),
-//         Container(
-//           margin: EdgeInsets.only(
-//           left: size.width*0.03,
-//           top: size.height*0.01,
-//           bottom: size.height*0.01,
-//           ),
-//           child: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             mainAxisAlignment: MainAxisAlignment.start,
-//             children: [
-//               Padding(
-//                 padding: EdgeInsets.only(
-//                 right: size.width*0.02,
-//                 ),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.start,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 mainAxisSize: MainAxisSize.max,
-//               children: [
-//                 Padding(
-//                   padding: const EdgeInsets.only(
-//                   bottom: 1,
-//       ),
-//     child: Text(
-//     elder.name,
-//     overflow: TextOverflow.ellipsis,
-//     textAlign: TextAlign.left,
-//     style: TextStyle(
-//     color: ColorConstant.black900,
-//     fontSize: 25,
-//     fontFamily: 'Roboto',
-//     fontWeight: FontWeight.w500,
-//     ),
-//     ),
-//     ),
-//
-//     ],
-//     ),
-//     ),
-//     //   Container(
-//     //   alignment: Alignment.center,
-//     //   height: size.height*0.1,
-//     //   width: size.width*0.4,
-//     //   padding: EdgeInsets.only(
-//     //     left: size.width*0.03,
-//     //     right: size.width*0.03,
-//     //   ),
-//     //   decoration: BoxDecoration(
-//     //     color: Colors.grey[400],
-//     //     borderRadius: const BorderRadius.all(Radius.circular(10)),
-//     //
-//     //   ),
-//     //   child: Text(
-//     //     service.name,
-//     //      style: const TextStyle(
-//     //       color: Colors.white,
-//     //     ),
-//     //   ),
-//     // );
-//   }
-//
-// }
-//
+import 'package:flutter/material.dart';
+
+import '../core/models/service_data_model.dart';
+import '../core/utils/color_constant.dart';
+
+class ServiceItemHomeWidget extends StatelessWidget{
+  ServiceDataModel service;
+  ServiceItemHomeWidget({super.key, required this.service});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: EdgeInsets.only(
+        top: size.height*0.01,
+        bottom: size.height*0.005,
+        left: size.width*0.03,
+        ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.only(
+              left: size.width*0.03,
+              top: size.height*0.01,
+              right: size.width*0.03,
+              bottom: size.height*0.01,
+              ),
+            decoration: BoxDecoration(
+              color: ColorConstant.gray400,
+              borderRadius: BorderRadius.circular(
+              40,
+              ),
+            ),
+          ),
+        Container(
+          margin: EdgeInsets.only(
+          left: size.width*0.03,
+          top: size.height*0.01,
+          bottom: size.height*0.01,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                right: size.width*0.02,
+                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                  bottom: 1,
+                  ),
+                  child: Text(
+                    service.name,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: ColorConstant.black900,
+                      fontSize: 25,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                ),
+                ],
+              ),
+              ),
+              ],
+            ),
+          ),
+          ],
+        ),
+      ),
+    );
+    //   Container(
+    //   alignment: Alignment.center,
+    //   height: size.height*0.1,
+    //   width: size.width*0.4,
+    //   padding: EdgeInsets.only(
+    //     left: size.width*0.03,
+    //     right: size.width*0.03,
+    //   ),
+    //   decoration: BoxDecoration(
+    //     color: Colors.grey[400],
+    //     borderRadius: const BorderRadius.all(Radius.circular(10)),
+    //
+    //   ),
+    //   child: Text(
+    //     service.name,
+    //      style: const TextStyle(
+    //       color: Colors.white,
+    //     ),
+    //   ),
+    // );
+  }
+
+}
+
