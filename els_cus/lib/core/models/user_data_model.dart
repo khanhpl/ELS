44101831b3732abc959086e38465d12f437a8f6a
binhquyen5769 <ngobinhkhien@gmail.com>
2022-11-19 14:48:09 +0700

    catch duplicate email and role

--- els_cus/lib/core/models/user_data_model.dart
+++ els_cus/lib/core/models/user_data_model.dart
@@ -1,13 +1,13 @@
 class UserDataModel {
-
   String email;
   String role;
   String token;
   String address;
-  DateTime? dob;
+  String dob;
   String fullName;
   String gender;
   String phone;
+
   UserDataModel({
     required this.email,
     required this.role,
@@ -19,30 +19,30 @@ class UserDataModel {
     required this.phone,
   });
 
-  setDob(DateTime? newDob){
+  setDob(String newDob) {
     dob = newDob;
   }
 
-
   factory UserDataModel.fromJson(Map<String, dynamic> json) => UserDataModel(
-    email: json["email"],
-    role: json["role"],
-    token: json["token"],
-    address: (json["address"]) != null?json["address"]:"",
-    dob: (json["dob"]  != null)?(DateTime.parse(json["dob"])): null,
-    fullName: json["fullName"],
-    gender: (json["gender"] != null)?json["gender"]:"",
-    phone: (json["phone"] != null)?json["phone"]:"",
-  );
+        email: json["email"],
+        role: json["role"],
+        token: json["token"],
+        address: (json["address"]) != null ? json["address"] : "",
+        dob: (json["dob"]) != null ? json["dob"] : "",
+        fullName: json["fullName"],
+        gender: (json["gender"] != null) ? json["gender"] : "",
+        phone: (json["phone"] != null) ? json["phone"] : "",
+      );
 
   Map<String, dynamic> toJson() => {
-    "email": email,
-    "role": role,
-    "token": token,
-    "address": address,
-    "dob": "${dob!.year.toString().padLeft(4, '0')}-${dob!.month.toString().padLeft(2, '0')}-${dob!.day.toString().padLeft(2, '0')}",
-    "fullName": fullName,
-    "gender": gender,
-    "phone": phone,
-  };
+        "email": email,
+        "role": role,
+        "token": token,
+        "address": address,
+        // "dob": "${dob!.year.toString().padLeft(4, '0')}-${dob!.month.toString().padLeft(2, '0')}-${dob!.day.toString().padLeft(2, '0')}",
+        "dob": dob,
+        "fullName": fullName,
+        "gender": gender,
+        "phone": phone,
+      };
 }
