555c4817a0a16a180c880a7ec5fdcc597a3581bd
khanhpl <khanh.phanluu01@gmail.com>
2022-11-19 23:17:09 +0700

    khanhpl

--- els_cus/lib/blocs/sign_up_blocs.dart
+++ els_cus/lib/blocs/sign_up_blocs.dart
@@ -3,63 +3,69 @@ import 'dart:convert';
 
 import 'package:els_cus_mobile/core/validators/validations.dart';
 import 'package:http/http.dart' as http;
-class SignUpBloc{
+
+class SignUpBloc {
   final StreamController _emailController = StreamController();
   final StreamController _passController = StreamController();
   final StreamController _fullnameController = StreamController();
   final StreamController _rePassController = StreamController();
+
   Stream get fullnameStream => _fullnameController.stream;
+
   Stream get rePassStream => _rePassController.stream;
+
   Stream get emailStream => _emailController.stream;
+
   Stream get passStream => _passController.stream;
-  bool isValidInput(String fullname, String email, String pass, String rePass){
+
+  bool isValidInput(String fullname, String email, String pass, String rePass) {
     bool isValid = false;
     bool fullnameValid = false;
     bool userValid = false;
     bool passValid = false;
     bool rePassValid = false;
-    if(fullname.isEmpty){
+    if (fullname.isEmpty) {
       _fullnameController.sink.addError("Họ và tên không thể để trống");
       fullnameValid = false;
-    }else{
+    } else {
       _fullnameController.sink.add("OK");
       fullnameValid = true;
     }
-    if(!Validations.isValidEmail(email)){
-
+    if (!Validations.isValidEmail(email)) {
       _emailController.sink.addError("Email không hợp lệ!");
       userValid = false;
-    }else{
+    } else {
       _emailController.sink.add("OK");
       userValid = true;
     }
-    if(!Validations.isValidPassword(pass)){
-      _passController.sink.addError("Mật khẩu phải có tối thiểu 8 ký tự và bao gồm chữ thường, chữ in hoa và số");
+    if (!Validations.isValidPassword(pass)) {
+      _passController.sink.addError(
+          "Mật khẩu phải có tối thiểu 8 ký tự và bao gồm chữ thường, chữ in hoa và số");
       passValid = false;
-    }else{
+    } else {
       _passController.sink.add("OK");
       passValid = true;
     }
 
-    if(pass != rePass){
+    if (pass != rePass) {
       _rePassController.sink.addError("Mật khẩu nhập lại không trùng khớp");
       rePassValid = false;
-    }else{
+    } else {
       _rePassController.sink.add("OK");
       rePassValid = true;
     }
-    if(userValid && passValid && fullnameValid && rePassValid){
+    if (userValid && passValid && fullnameValid && rePassValid) {
       isValid = true;
     }
     return isValid;
   }
+
   Future<bool> createCus(String fullname, String email, String password) async {
     try {
-      print('test fullname: '+ fullname);
-      print('test email:'+ email);
+      print('test fullname: ' + fullname);
+      print('test email:' + email);
       print('Test password:' + password);
-      var url =
-      Uri.parse("https://els12.herokuapp.com/customer/add");
+      var url = Uri.parse("https://els12.herokuapp.com/customer/add");
       final response = await http.post(
         url,
         headers: <String, String>{
@@ -73,10 +79,12 @@ class SignUpBloc{
           },
         ),
       );
-      // print('Status: ' + response.statusCode.toString());
       if (response.statusCode.toString() == '200') {
-
-        return true;
+        if (json.decode(response.body)["errorCode"] != "DUPLICATE_EMAIL") {
+          return true;
+        } else {
+          return false;
+        }
       } else {
         return false;
       }
